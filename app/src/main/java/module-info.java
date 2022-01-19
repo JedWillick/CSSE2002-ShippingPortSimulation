6ef84ed1ec54195a6617df44c17782c01ca90708
Jed Willick <jwillick@me.com>
2022-01-19 17:03:33 +1000

    Fixed the module-info and javadocs

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,4 +1,22 @@
-module portsim.app.main {
+/**
+ * <b>Shipping Port Simulation</b>
+ * <br>
+ * Assignment 1 &amp; 2 (combined) for CSSE2002, Semester 2 2021
+ * <br><br>
+ * <b>Acknowledgment</b>:
+ * <ul>
+ *     <li>
+ *         The majority of the JavaDoc in portsim was taken from the provided
+ *         <a href="https://csse2002.uqcloud.net/assignment/2/index.html">specification</a>.
+ *     </li>
+ *     <li>
+ *         All code marked with {@code @given} was provided by course staff.
+ *     </li>
+ * </ul>
+ *
+ * @author Jed Willick
+ */
+open module portsim.app.main {
+    requires transitive javafx.graphics;
     requires javafx.controls;
-    opens portsim to javafx.graphics;
 }
